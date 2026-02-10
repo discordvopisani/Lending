8be8eb93577e0f92a99fdb456e7781ac54273021
discordvopisani <medeuumirbaev2006@gmail.com>
2026-02-10 14:50:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-function login() { const email = document.getElementById("email").value; if (email !== "") { localStorage.setItem("user", email); // "запоминаем" вход window.location.href = "index.html"; // переход } else { alert("Введите email"); } } function choosePlan(plan) { localStorage.setItem("plan", plan); alert("Вы выбрали: " + plan); window.location.href = "index.html"; }
+function login() 
+{ const email = document.getElementById("email").value; 
+ if (email !== "") { localStorage.setItem("user", email); // "запоминаем" вход window.location.href = "index.html"; // переход } 
+ else alert("Введите email"); } } 
+function choosePlan(plan)
+{ localStorage.setItem("plan", plan); alert("Вы выбрали: " + plan); window.location.href = "index.html"; }
