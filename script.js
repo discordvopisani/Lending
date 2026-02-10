dc8537885c9a0bd9c05723f429d286674cae31dc
discordvopisani <medeuumirbaev2006@gmail.com>
2026-02-10 14:51:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function login()
  else alert("Введите email"); } } 
 function choosePlan(plan)
 { localStorage.setItem("plan", plan); alert("Вы выбрали: " + plan); window.location.href = "index.html"; }
+function buy(period, price) {
+  const text =
+    `Сәлеметсіз бе! Мен StreamX подпискасын сатып алғым келеді.\n` +
+    `Тариф: ${period} — ${price} тг`;
+
+  const url =
+    "https://wa.me/77752631158?text=" + encodeURIComponent(text);
+
+  window.open(url, "_blank");
+}
+
